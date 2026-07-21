a4948c6f0c12a4de356d8c482d52650c26e60e1e
andychittock1 <andy@conversio.com>
2026-07-21 12:51:38 +0100

    new file:   conversio_session_initiator_v1.4.js

---
+++ conversio_session_initiator_v1.4.js
@@ -0,0 +1,10 @@
+//<script>
+// CONVERSIO SESSION INITIATION TAG | Client: XXXXXX : version 1.4 - updated 26-05-2026
+(function () {
+  'use strict';
+
+  if (typeof window.__conversioEnableEmission__ === 'function') {
+    window.__conversioEnableEmission__();
+  }
+})();
+//</script>
